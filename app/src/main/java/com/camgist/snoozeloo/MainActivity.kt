5ac7cfe33c84eb0a7bfd759fb4efd65a811d2892
JEANNOEL <nfonjeannoel1@gmail.com>
2024-11-05 17:56:48 +0100

    AlarmDetailsScreen init

--- app/src/main/java/com/camgist/snoozeloo/MainActivity.kt
+++ app/src/main/java/com/camgist/snoozeloo/MainActivity.kt
@@ -6,47 +6,54 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.camgist.snoozeloo.alarm.presentation.alarm_details.AlarmDetailsScreen
+import com.camgist.snoozeloo.alarm.presentation.alarm_list.AlarmListScreen
+import com.camgist.snoozeloo.ui.theme.MyDimensions
 import com.camgist.snoozeloo.ui.theme.SnoozelooTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen().setKeepOnScreenCondition() {
-            // keep on forever for debugging purposes
-            true
-        }
+        installSplashScreen()
+//            .setKeepOnScreenCondition() {
+//            // keep on forever for debugging purposes
+//            true
+//        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SnoozelooTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+//                    modifier = Modifier.fillMaxSize()
+//                        .padding(bottom = MyDimensions.smallPadding),
+//                    floatingActionButton = {
+//                        FloatingActionButton(
+//                            onClick = { /*TODO*/ },
+//                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                            shape = CircleShape,
+//
+//                        ) {
+//                            Icon(Icons.Filled.Add, "Add")
+//                        }
+//                    },
+//                    floatingActionButtonPosition = FabPosition.Center
+                ) { innerPadding ->
+//                    AlarmListScreen(modifier = Modifier.padding(innerPadding))
+                    AlarmDetailsScreen(modifier = Modifier.padding(innerPadding))
+
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SnoozelooTheme {
-        Greeting("Android")
-    }
-}
